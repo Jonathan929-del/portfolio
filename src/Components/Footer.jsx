1a34d99a78749d4fef2b89b2ef32880e0851b3b3
Jonathan Adel <joathanadel98@gmail.com>
2022-03-27 19:07:38 +0200

    Commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -16,6 +16,20 @@ const FooterContainer = styled.footer`
     justify-content:center;
     color:${({styleDarkMode}) => styleDarkMode === 'true' ? '#fff' : '#000'};
     background-color:${({styleDarkMode}) => styleDarkMode === 'true' ? '#1d1a22' : '#ebebeb'};
+
+    @media screen and (max-width:600px){
+      height:70px;
+      font-size:12px;
+    }
+
+    @media screen and (max-width:400px){
+      font-size:10px;
+    }
+
+    @media screen and (max-width:330px){
+      height:50px;
+      font-size:8px;
+    }
 `
 
 const Name = styled.p`
